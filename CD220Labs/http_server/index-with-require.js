aa6d4323cd1dcadeb42a45abf1245282102ecaf8
Ahmedns09 <anskhzyan@gmail.com>
2025-09-02 07:03:51 -0400

    added time zone for uae in the today.js module

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,4 +1,4 @@
- // Import the HTTP module
+// Import the HTTP module
 const http = require('http');
 
 // Import the 'today' module
@@ -8,7 +8,7 @@ const today = require('./today');
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
     // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+    res.end(`Hello, World! The date today is ${today.getDate()} \n`);
 };
 
 // Define the port number
